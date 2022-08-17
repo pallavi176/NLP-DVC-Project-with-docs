e280edbf133f8753fded294dc2537d63ea69b55d
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-08-17 21:29:19 +0530

    stage 2 initial changes

---
+++ research_trial_dir/Count_vect.py
@@ -0,0 +1,45 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+# corpus = [
+#     "zebra apple ball cat cat",
+#     "ball cat dog elephant",
+#     "very very unique"
+# ]
+
+# vectorizer = CountVectorizer()
+# X = vectorizer.fit_transform(corpus)
+# print(X.toarray())
+# print(vectorizer.get_feature_names_out())
+
+corpus = [
+    "apple ball cat",
+    "ball cat dog elephant",
+]
+
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+
+
+max_features = 4
+ngrams = 2 # tri gram
+
+vectorizer = CountVectorizer(max_features=max_features, ngram_range=(1, ngrams))
+X = vectorizer.fit_transform(corpus)
+print(X.toarray())
+print(vectorizer.get_feature_names_out())
+
+# corpus = [
+#     "apple ball cat",
+#     "ball cat dog",
+# ]
+
+# ['apple' 
+# 'apple ball' 
+# 'apple ball cat' 
+# 'ball' 
+# 'ball cat' 
+# 'ball cat dog'
+# 'cat' 
+# 'cat dog' 
+# 'dog']
